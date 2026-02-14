c569a3d59a25ab5dbc6151b80cc0518ab47f17e6
deniscerri <64997243+deniscerri@users.noreply.github.com>
2026-02-14 20:24:30 +0100

    .

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-## YTDLNIS PLUGINS
+## YTDLnis Packages
 
-To build locally
+# General information
+
+This repository is built for storage and documentation purposes.
+Each package is a shell app that simply contains jniLibs within them. YTDLnis can then access them based on their package name to import the zip into its own file system to execute them.
+This method also bypasses the SDK28 restriction, that restricts executing executables that are not pre-bundled within the app in jniLibs folder.
+
+By using helper apks, the libxxx.so is extracted from jniLibs from the helper apk and its usable to any app in the device as long as you know the package name.
+libxxx.so is a just a binary that executes contents in libxxx.zip.so
+libxxx.zip.so can be read by other apps as long as you know the package name, so you can copy and unzip its contents to the app's filesystem.
+Then you can make helper apk's libxxx.so execute the extracted zip containing the usr/ files. 
+
+You could also inject the jniLibs files directly in the app without apk helpers, but you have to downgrade to target sdk28 to achieve this.
+
+Building termux-packages takes an excessive amount of time, and github workflows has a time limit, so it is impossible to build it within github, so i am building the packages locally and then manually publishing new versions
+
+# To build jniLibs locally
 
 - clone latest version of termux-packages repo
 - pull the latest version of termux/package-builder docker image
---
+++ python_package/app/build.gradle.kts
@@ -0,0 +1,77 @@
+import com.android.build.api.variant.FilterConfiguration
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+val versionMajor = 3
+val versionMinor = 12
+val versionPatch = 11
+
+val abiCodes = mapOf(
+    "armeabi-v7a" to 1,
+    "arm64-v8a" to 2,
+    "x86" to 3,
+    "x86_64" to 4
+)
+
+android {
+    namespace = "com.deniscerri.ytdl.python"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.deniscerri.ytdl.python"
+        minSdk = 24
+        targetSdk = 36
+
+        // Base version code
+        versionCode = versionMajor * 1000000 + versionMinor * 10000 + versionPatch * 100
+        versionName = "$versionMajor.$versionMinor.$versionPatch"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
+    //noinspection WrongGradleMethod
+    androidComponents {
+        onVariants { variant ->
+            variant.outputs.forEach { output ->
+                val abiName = output.filters.find {
+                    it.filterType == FilterConfiguration.FilterType.ABI
+                }?.identifier
+
+                val abiCode = abiCodes[abiName] ?: 0
+                val baseCode = versionMajor * 1000000 + versionMinor * 10000 + versionPatch * 100
+
+                // Set the version code directly using the base code + ABI offset
+                output.versionCode.set(baseCode + abiCode)
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+}
